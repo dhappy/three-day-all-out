ca0836d3faf40efc6b8dcbc7515cfbdce222b454
W.J. “dysbulic” H <dys@dhappy.org>
2022-09-05 23:41:29 -0400

    switchin' to PostCSS & addin' rocks logo 🧩

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.pcss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <body>
+      <header>
+        <button>Sign-In With Ethereum</button>
+      </header>
+
+      <main>
+        <section>
+          <h1>Create An Availability</h1>
+
+          <p>These are markers for times that the given group is available.</p>
+
+          <form>
+            <fieldset className="flex">
+              <legend>Applicable Days</legend>
+              <fieldset className="inline-block">
+                <legend>Start Date</legend>
+                <input type="date" name="date.start"/>
+              </fieldset>
+              <fieldset className="inline-block">
+                <legend>End Date</legend>
+                <input type="date" name="date.end"/>
+              </fieldset>
+            </fieldset>
+            <fieldset>
+              <legend>Start Time</legend>
+              <label htmlFor="start.earliest">Earliest Start Time</label>
+              <input type="time" name="start.earliest"/>
+              <label htmlFor="start.latest">Latest Start Time</label>
+              <input type="time" name="start.latest"/>
+            </fieldset>
+            <fieldset>
+              <legend>End Time</legend>
+              <label htmlFor="end.earliest">Earliest End Time</label>
+              <input type="time" name="end.earliest"/>
+              <label htmlFor="end.latest">Latest End Time</label>
+              <input type="time" name="end.latest"/>
+            </fieldset>
+          </form>
+        </section>
+      </main>
+    </body>
   )
 }
 
